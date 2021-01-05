6426ad54795b1b9fc301f1e25bfef29c1d7185d0
Rajanish <rajanishatwork@gmail.com>
2021-01-06 01:47:36 +0530

    api context path changes
    Signed-off-by: Rajanish <rajanishatwork@gmail.com>

--- src/main/java/com/shopskill/anubis/rest/controller/UserRestController.java
+++ src/main/java/com/shopskill/anubis/rest/controller/UserRestController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
+@SuppressWarnings("unused")
 public class UserRestController {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
